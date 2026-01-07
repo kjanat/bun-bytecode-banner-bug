2ed7a23d0792d210bda76a166ae54a827749d3a4
Kaj Kowalski <info@kajkowalski.nl>
2026-01-07 15:23:02 +0100

    docs: include build output for documentation

---
+++ out/no-shebang.js
@@ -0,0 +1,4 @@
+// @bun @bytecode @bun-cjs
+(function(exports, require, module, __filename, __dirname) {// src/no-shebang.ts
+console.log("Hello from bytecode!");
+})
---
+++ out/shebang.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env bun
+// @bun @bytecode @bun-cjs
+(function(exports, require, module, __filename, __dirname) {
+// src/shebang.ts
+console.log("Hello from bytecode!");
+})
